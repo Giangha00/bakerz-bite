764fa7e023793384982ac96f19bbbf22bab2e8cf
macbook <gianghacd@gmail.com>
2025-07-17 13:22:34 +0700

    upcode

---
+++ src/ult/url.js
@@ -0,0 +1,12 @@
+const URL = {
+  BASE_URL: "http://localhost:8888/api",
+  DETAIL_PRODUCT: "/detail_products.php?id=",
+  CATEGORY_PRODUCTS: "/category_products.php?category_id=",
+  CATEGORY_LIST: "/categories.php",
+  CREATE_ORDER: "/create_order.php",
+  UPDATE_ORDER: "/update_order.php",
+  FEED_BACK: "/feed_back.php",
+  CREATE_FEED_BACK: "/create_feed_back.php",
+};
+
+export default URL;
