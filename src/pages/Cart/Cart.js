458f38bc38f1e230749a9bfc82b786751420d2fb
TamNguyennn3010 <prykay3010@gmail.com>
2025-07-19 20:31:19 +0700

    Tam keo code ve

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,5 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../Cart/Cart.css";
 const Cart = () => {
-  return <h1>Cart</h1>;
+  return (
+    <div className="cart-content">
+      <div className="cart-header">
+        <FontAwesomeIcon
+          icon="fa-solid fa-cart-shopping"
+          className="cart-icon"
+        />
+      </div>
+      <h1>Your Cart Is Empty</h1>
+    </div>
+  );
 };
 
 export default Cart;
--- src/pages/Merchandise/Merchandise.js
+++ src/pages/Merchandise/Merchandise.js
@@ -1,6 +1,47 @@
+import axios_instance from "../../ult/axios_instance";
+import URL from "../../ult/url";
 import "../Merchandise/Merchandise.css";
+import React, { useEffect, useState } from "react";
+
 const Merchandise = () => {
-  return <h1>Merchandise</h1>;
+  const [state, setState] = useState([]);
+
+  const getMerchandise = async () => {
+    const rs = await axios_instance.get(URL.MERCHANDISE_LIST);
+    const data = rs.data.data;
+    setState(data);
+  };
+
+  useEffect(() => {
+    getMerchandise();
+  }, []);
+
+  return (
+    <div className="merchandise-content">
+      <div className="merchandise-title">
+        <h1>Merchandise</h1>
+        <p>
+          Take home a piece of Bakerz Bite with our branded merchandise
+          collection
+        </p>
+      </div>
+      <div className="merchandise-cart">
+        {state.map((p) => (
+          <div key={p.id} className="merchandise-cart-content">
+            <img
+              src={p.image}
+              alt={p.title}
+              className="merchandise-cart-image"
+            />
+            <p className="merchandise-cart-price">{p.price}$</p>
+            <p className="merchandise-cart-name">{p.name}</p>
+            <p className="merchandise-cart-des">{p.description}</p>
+            <button className="merchandise-cart-btn">Add to cart</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Merchandise;
