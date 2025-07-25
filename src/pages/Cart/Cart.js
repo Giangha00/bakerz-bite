692ae6f7723ebbba0bec24716daaa97f0e3172fb
TamNguyennn3010 <prykay3010@gmail.com>
2025-07-25 18:40:16 +0700

    Tam up code

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,15 +1,24 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CiShoppingCart } from "react-icons/ci";
 import "../Cart/Cart.css";
+import { Link } from "react-router-dom";
+
 const Cart = () => {
+  const cartData = localStorage.getItem("state");
+  console.log("cartData", cartData);
   return (
     <div className="cart-content">
-      <div className="cart-header">
-        <FontAwesomeIcon
-          icon="fa-solid fa-cart-shopping"
-          className="cart-icon"
-        />
+      <div>
+        <CiShoppingCart className="cart-icon" />
       </div>
-      <h1>Your Cart Is Empty</h1>
+      <div className="cart">
+        <h1 className="cart-content-title">Your Cart Is Empty</h1>
+        <p className="cart-content-des">
+          Looks like you haven't added any delicious items to your cart yet.
+        </p>
+        <Link to="/">
+          <button className="cart-content-btn">Start shopping</button>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/pages/Merchandise/Merchandise.js
+++ src/pages/Merchandise/Merchandise.js
@@ -1,48 +1,85 @@
+import { useContext, useEffect, useState } from "react";
 import axios_instance from "../../ult/axios_instance";
 import URL from "../../ult/url";
+import UserContext from "../../context/context";
 import "../Merchandise/Merchandise.css";
-import React, { useEffect, useState } from "react";
 
 const Merchandise = () => {
-  const [state, setState] = useState([]);
+  const { state, dispatch } = useContext(UserContext);
+  const [products, setProducts] = useState([]);
 
-  const getMerchandise = async () => {
-    const rs = await axios_instance.get(URL.MERCHANDISE_LIST);
-    const data = rs.data.data;
-    setState(data);
+  const getProducts = async () => {
+    try {
+      const response = await axios_instance.get(URL.GET_PRODUCTS);
+      const data = await response.data.data;
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
   };
 
   useEffect(() => {
-    getMerchandise();
+    getProducts();
   }, []);
 
+  const addToCart = (p) => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: {
+        id: p.id,
+        category_id: p.category_id,
+        name: p.name,
+        qty: 1,
+        image: p.thumbnail,
+        price: p.price,
+      },
+    });
+  };
+
   return (
-    <div className="merchandise-content">
-      <div className="merchandise-title">
-        <h1>Merchandise</h1>
-        <p>
+    <>
+      <div className="merchandise-content">
+        <h1 className="merchandise-title">Merchandise</h1>
+        <p className="merchandise-des">
           Take home a piece of Bakerz Bite with our branded merchandise
           collection
         </p>
+        <div className="merchandise-products">
+          {Array.isArray(products) &&
+            products.map((product) => {
+              if (product.type === "Merchandise") {
+                return (
+                  <div className="merchandise-cart">
+                    <div key={product.id}>
+                      <p className="merchandise-cart-price">
+                        Price: ${product.price}
+                      </p>
+                      <img
+                        className="merchandise-cart-img"
+                        src={product.thumbnail}
+                        alt={product.name}
+                      />
+                      <h3 className="merchandise-cart-name">{product.name}</h3>
+                      <p className="merchandise-cart-des">
+                        {product.description}
+                      </p>
+
+                      <button
+                        className="merchandise-cart-btn"
+                        onClick={() => addToCart(product)}
+                      >
+                        Add to Cart
+                      </button>
+                    </div>
+                  </div>
+                );
+              }
+              return null;
+            })}
+        </div>
       </div>
-      <div className="merchandise-cart">
-        {state.map((p) => (
-          <div key={p.id} className="merchandise-cart-content">
-            <img
-              src={p.image}
-              alt={p.title}
-              className="merchandise-cart-image"
-            />
-            <p className="merchandise-cart-price">{p.price}$</p>
-            <p className="merchandise-cart-name">{p.name}</p>
-            <p className="merchandise-cart-des">{p.description}</p>
-            <button className="merchandise-cart-btn">Add to cart</button>
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
 export default Merchandise;
-//TamNguyen
