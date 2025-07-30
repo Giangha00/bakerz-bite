4f56e5caab759e74545ef97c276dbc2ec8e4770e
TamNguyennn3010 <prykay3010@gmail.com>
2025-07-30 23:01:28 +0700

    tam up code

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,17 +1,74 @@
 import { CiShoppingCart } from "react-icons/ci";
+import { MdOutlineRemoveShoppingCart } from "react-icons/md";
 import "../Cart/Cart.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import UserContext from "../../context/context";
+import { width } from "@fortawesome/free-regular-svg-icons/faAddressBook";
 
 const Cart = () => {
   const { state, dispatch } = useContext(UserContext);
-  // const cart = JSON.parse(localStorage.getItem("state")) || [];
+  const cartItems = state.cart;
+  const increaseQty = (id) => {
+    dispatch({
+      type: "INCREASE_QTY",
+      payload: id,
+    });
+  };
 
+  const decreaseQty = (id) => {
+    dispatch({
+      type: "DECREASE_QTY",
+      payload: id,
+    });
+  };
   return (
     <div className="cart-content">
       {state.cart.length !== 0 ? (
-        <div>new view</div>
+        <div>
+          {cartItems.map((item, index) => (
+            <div key={index} className="cart-item">
+              <div className="cart-item-start">
+                <img
+                  className="cart-item-image"
+                  src={item.image}
+                  alt={item.name}
+                />
+                <div>
+                  <div className="cart-item-name">{item.name}</div>
+                  <div className="cart-item-price">${item.price}</div>
+                </div>
+              </div>
+              <div className="cart-item-total">
+                <div className="total">${item.total_price}</div>
+                <div className="cart-item-qty">
+                  <button
+                    onClick={() => increaseQty(item.id)}
+                    className="button-qty"
+                  >
+                    +
+                  </button>
+                  {item.qty}
+                  <button
+                    onClick={() => decreaseQty(item.id)}
+                    className="button-qty"
+                  >
+                    -
+                  </button>
+                  <div
+                    onClick={() =>
+                      dispatch({ type: "REMOVE_CART", payload: item.id })
+                    }
+                  >
+                    <MdOutlineRemoveShoppingCart
+                      style={{ width: "25px", height: "25px", color: "red" }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       ) : (
         <>
           <div>
@@ -23,7 +80,7 @@ const Cart = () => {
               Looks like you haven't added any delicious items to your cart yet.
             </p>
             <Link to="/">
-              <button className="cart-content-btn">Start shopping</button>
+              <button className="cart-content-btn">Continue shopping</button>
             </Link>
           </div>
         </>
