835013b8221eaadf3a3c93021b939f236cf0fa44
TamNguyennn3010 <prykay3010@gmail.com>
2025-08-08 21:26:30 +0700

    Tam up code

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -39,7 +39,7 @@ const Home = () => {
   const fetchProducts = useCallback(async () => {
     try {
       const response = await fetch(
-        `http://localhost:8888/api/products.php?min_price=${minPrice}&max_price=${maxPrice}`
+        `http://localhost/api/products.php?min_price=${minPrice}&max_price=${maxPrice}`
       );
       if (!response.ok) throw new Error("Network response was not ok");
 
